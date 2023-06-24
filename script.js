1f733053972f6717f6c9aff0b71fd90d00e5057a
Yassine <132377798+yassinemahboub@users.noreply.github.com>
2023-06-24 16:11:39 +0100

    Get Data With Fetch

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // -- GLOBAL --
+const MAX_CHARS = 150;
+
+
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
 const submitBtnEl = document.querySelector('.submit-btn')
+const spinnerEl = document.querySelector('.spinner')
 
 
 // -- COUNTER COMPONANTS --
@@ -11,7 +16,7 @@ const submitBtnEl = document.querySelector('.submit-btn')
 
 const inputHandler = () => {
     // Determine maximum number of characters
-     const maxNrChars = 150;
+     const maxNrChars = MAX_CHARS;
     // Detemine number of characters currently typed
     const nrCharsTyped = textareaEl.value.length;
     // Calculate number of characters left 
@@ -24,8 +29,17 @@ const inputHandler = () => {
 
 textareaEl.addEventListener('input', inputHandler)
 
-// -- SUBMIT COMPONENTS --
+// -- FORM COMPONENTS --
 
+const showVisualIndicator = textCheck => {
+    const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
+    
+    
+    formEl.classList.add(className);
+    setTimeout(() => {
+      formEl.classList.remove(className)  
+    }, 2000); 
+}
 
 const submitHandler = event => {
     // prevent default browser action "submitting form to 'action' -address and loading new page"
@@ -34,15 +48,9 @@ const submitHandler = event => {
     const text = textareaEl.value;
     // validate text
     if (text.includes('#') && text.length >= 5) {
-        formEl.classList.add('form--valid');
-        setTimeout(() => {
-          formEl.classList.remove('form--valid')  
-        }, 2000);
+        showVisualIndicator('valid');
     } else {
-        formEl.classList.add('form--invalid');
-        setTimeout(() => {
-            formEl.classList.remove('form--invalid')  
-          }, 2000);
+        showVisualIndicator('invalid');
     // focus textarea
     textareaEl.focus();
     //stop this function execution
@@ -80,9 +88,47 @@ const submitHandler = event => {
     // blur submit utton
     submitBtnEl.blur();
     // reset counter
-    counterEl.textContent = '150';
+    counterEl.textContent =  MAX_CHARS;
 };
 
 
 
 formEl.addEventListener('submit', submitHandler)
+
+// -- FEEDBACK LIST COMPONENT --
+fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks').then(response => {
+    return response.json();
+}).then(data => {
+        // remove spinner
+        spinnerEl.remove();
+        // iterate over each element in feedback array and render it in list
+        data.feedbacks.forEach(feedbackItem => {
+            // new feedback item HTML
+            const feedbackItemHTML = `
+            <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${feedbackItem.company}</p>
+                <p class="feedback__text">${feedbackItem.text}</p>
+            </div>
+            <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+            </li>
+            `;
+
+            //insert new feedback item in list 
+            feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+            });
+        })
+        .catch(error => {
+           feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
+        });
+
+
+
+    
